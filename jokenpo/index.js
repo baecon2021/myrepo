3ba42b14ee3b047da289cae4716c2b4479800937
anthony <anthonyvelho.baecon@gmail.com>
2023-04-17 21:15:56 -0300

    BIG CHANGES

---
+++ jokenpo/index.js
@@ -0,0 +1,47 @@
+const playerText = document.querySelector("#Playertext");
+const computerText = document.querySelector("#Computertext");
+const resultText = document.querySelector("#Resulttext");
+const choiceBtns = document.querySelectorAll(".choicebtn")
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computerTurn()
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computador: ${computer}`;
+    resultText.textContent = checkWin();
+}));
+
+function computerTurn(){
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+        case 1:
+            computer = "PEDRA";
+            break;
+        case 2:
+            computer = "PAPEL";
+            break;
+        case 3:
+            computer = "TESOURA";
+    }
+}
+
+function checkWin(){
+    if (player == computer){
+        return "Empate!";
+    }
+    else if (computer == "PEDRA"){
+        return (player == "PAPEL") ? "Voce venceu!" : "Voce Perdeu!";
+    }
+    else if (computer == "PAPEL"){
+        return (player == "TESOURA") ? "Voce venceu!" : "Voce perdeu!";
+    }
+    else if (computer == "TESOURA"){
+        return (player == "PEDRA") ? "voce venceu!" : "Voce perdeu!"
+    }
+}
